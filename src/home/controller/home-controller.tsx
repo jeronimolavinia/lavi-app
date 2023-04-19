9db8b9877c79b464efd10ef725f920fdec136fa8
Lavinia Cirilo Jeronimo <FRI0026031@aluno.firjansenaisesi.com.br>
2023-04-18 21:14:19 -0300

    18-04

---
+++ src/home/controller/home-controller.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeView from "../view/home-view";
+
+export default function HomeController(){
+    return(
+        <HomeView/>
+    )
+
+
+}
---
+++ src/home/view/home-view.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ButtonComponent from './components/button-component';
+
+export default function HomwView(){
+  return (
+    <div>
+        <ButtonComponent>
+
+        </ButtonComponent>
+    </div>
+  );
+}
+
+
